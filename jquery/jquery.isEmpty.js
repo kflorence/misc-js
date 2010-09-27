2132529dd6e83e75a02ebbbf94518fefdefb7689
Kyle Florence <kyle.florence@gmail.com>
2010-09-27 14:24:27 -0700

    add jQuery.isEmpty method

---
+++ jquery/jquery.isEmpty.js
@@ -0,0 +1,26 @@
+/**
+ * @fileOverview The jQuery.isEmpty plugin.
+ * @author <a href="mailto:kyle.florence@gmail.com">Kyle Florence</a>
+ * @version 1.0
+ */
+
+(function($) {
+    /**
+     * @param Mixed o The argument to test against.
+     * @return Boolean Returns true if argument is empty, false otherwise.
+     */
+    $.isEmpty = function(o) {
+        if ($.isArray(o) || typeof o === "string") {
+            return (o.length ? false : true);
+        } else if (typeof o === "object") {
+            for (var p in o) {
+                if (o.hasOwnProperty(p)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return (o ? false : true);
+        }
+    };
+})(jQuery);
