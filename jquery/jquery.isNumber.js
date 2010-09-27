8519b00570c13044ae25d14172522fc124f42e1d
Kyle Florence <kyle.florence@gmail.com>
2010-09-27 14:24:42 -0700

    add jQuery.isNumber method

---
+++ jquery/jquery.isNumber.js
@@ -0,0 +1,15 @@
+/**
+ * @fileOverview The jQuery.isNumber plugin.
+ * @author <a href="mailto:kyle.florence@gmail.com">Kyle Florence</a>
+ * @version 1.0
+ */
+
+(function($) {
+    /**
+     * @param Mixed n The argument to test against.
+     * @return Boolean Returns true if argument is a finite number, false otherwise.
+     */
+    $.isNumber = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+})(jQuery);
